7451ec29b718e046619d8d4ac41dd5e09988285a
Valentin B <valentin.be@protonmail.com>
2022-05-07 23:21:12 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+//! A static reflection library for Rust, designed to resemble
+//! KingsIsle's *ObjectProperty* system.
+//!
+//! This is spiritually inspired by the original implementation
+//! from [Richard Lyle] as part of the [Medusa] project.
+//!
+//! [Richard Lyle]: https://github.com/rlyle
+//! [Medusa]: https://github.com/palestar/medusa
+
+#![deny(
+    rust_2018_idioms,
+    missing_docs,
+    rustdoc::broken_intra_doc_links,
+    unsafe_op_in_unsafe_fn
+)]
